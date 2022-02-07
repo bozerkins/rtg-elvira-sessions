e827b8011d5745a7cfddd5b240116fc01d1b5275
Bogdans Ozerkins <b.ozerkins@dynatech.lv>
2022-02-07 17:29:48 +0200

    task 7 solution

--- tasks-1-8/task7.js
+++ tasks-1-8/task7.js
@@ -1,3 +1,26 @@
-//7. Write a program that prints a multiplication table for numbers up to 12.
+//7. Write a program that prints a multiplication 
+// table for numbers up to 12.
 
-//any comment
+for(let row = 1; row <= 12; row++) {
+    let line = '';
+    for(let col = 1; col <= 12; col++) {
+        if (col === 1) {
+            line += (row * col);    
+        } else {
+            line += "\t" + (row * col);
+        }
+    }
+    console.log(line);
+}
+
+// for (let row = 1; row <= 12; row++) {
+//     let line = [];
+//     for (let col = 1; col <= 12; col++) {
+//         line.push(row * col);
+//     }
+//     let lineNicePrint = line.join("\t");
+//     console.log(lineNicePrint);
+// }
+
+
+// TODO: show variant with object
